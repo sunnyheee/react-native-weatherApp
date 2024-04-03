1050fa368e871cfaca655ffc950cdcf6891ab9a7
sunnyheee <fool0116@naver.com>
2024-04-03 22:01:12 +0900

    location 취득

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
+import React, { useState, useEffect } from "react";
+
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+
+import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loadding...");
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const ask = async () => {
+    // user의 위치 취득
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>東京</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
